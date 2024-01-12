131c145b554fa49a4476de7e5336ac54e23dd694
Nevra Nur Doğan <122152162+NevraNurDogan@users.noreply.github.com>
2024-01-12 23:59:33 +0300

    Ağaç eklendi.

---
+++ Ağaç/AVL_Tree/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    /* Sol alt ağaç,sağ alt ağaçtan küçük olmalı.
+    Sol ve sağ alt ağaç arasındaki yükseklik farkı en fazla 1 olmalı.
+    Balans
+    ---------------------
+    Sol fazla ise:1
+    Sağ fazla ise:-1
+    Eşitse:0
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
